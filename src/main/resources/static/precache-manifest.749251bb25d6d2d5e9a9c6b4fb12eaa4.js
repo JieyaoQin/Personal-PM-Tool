7e0349241a5cbfba3a129a3e067b80242abb6030
Jieyao <jq2261@columbia.edu>
2019-08-20 21:53:36 -0700

    added static contents

---
+++ src/main/resources/static/precache-manifest.749251bb25d6d2d5e9a9c6b4fb12eaa4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14003ee02ea57a69d743dbf20c5dd488",
+    "url": "/index.html"
+  },
+  {
+    "revision": "500100bda4b2bbbd7647",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "1d8c0da929b6339c9fa1",
+    "url": "/static/css/main.abdc95a3.chunk.css"
+  },
+  {
+    "revision": "500100bda4b2bbbd7647",
+    "url": "/static/js/2.a807eb96.chunk.js"
+  },
+  {
+    "revision": "1d8c0da929b6339c9fa1",
+    "url": "/static/js/main.c9ee9c37.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
